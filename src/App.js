00daa942493bd75e6d11bc7993c999d706a07f2e
Noman01 <neamulhaque2002@gmail.com>
2022-06-01 00:30:13 +0600

    components added and navbar edited

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import ContactUs from './components/ContactUs/ContactUs';
 import Home from './components/Home/Home';
+import OurTeam from './components/OurTeam/OurTeam';
+import Portfolio from './components/Portfolio/Portfolio';
 import Footer from './components/Shared/Footer';
 import Navbar from './components/Shared/Navbar';
+import NotFound from './components/Shared/NotFound'
 
 function App() {
   return (
@@ -15,6 +19,11 @@ function App() {
         <div className='max-w-[1280px]  '>
           <Routes>
             <Route path='/' element={<Home />}></Route>
+            <Route path='home' element={<Home />}></Route>
+            <Route path='portfolio' element={<Portfolio />}></Route>
+            <Route path='ourteam' element={<OurTeam />}></Route>
+            <Route path='contactus' element={<ContactUs />}></Route>
+            <Route path='*' element={<NotFound />}></Route>
           </Routes>
         </div>
       </div>
