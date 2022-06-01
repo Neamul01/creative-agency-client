ca397ae9db0e7d020d37bb4816c52482dafa8101
Noman01 <neamulhaque2002@gmail.com>
2022-06-01 22:59:54 +0600

    services title added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
 import Companys from './Companys';
+import Services from './Services';
 
 const Home = () => {
     return (
         <div className=' absolute top-0 z-10 w-full'>
             <Banner />
             <Companys />
+            <Services />
         </div>
     );
 };
---
+++ src/components/Home/Services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div className='flex justify-center my-28'>
+            <h2 className="text-4xl font-bold">Our Ausome <span className='text-[#7AB259]'> services</span></h2>
+        </div>
+    );
+};
+
+export default Services;
